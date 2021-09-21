126ad6d6960205a0f4fae5f6aea971c803ff7b46
James Sentinella <james@sentinella.me>
2021-09-21 14:42:33 -0700

    Account page looks about to be where I want it as far as looks go.

--- src/components/Parts/KeyInfo.js
+++ src/components/Parts/KeyInfo.js
@@ -8,15 +8,14 @@ import gear from '../../img/gear.svg';
 const KeyInfo = (keys) => {
     return (
         <div className="key-info-container">
-            <h2>Keys</h2>
+            <h2 className="text-center">Keys</h2>
             {
                 keys.map(key => {
                     const keyVal = key.value;
                     return (
-                        <div className="key-info" key={key.value}>
+                        <div className="key-info text-center" key={key.value}>
                             <Formik
                                 initialValues={{
-                                    value: key.value,
                                     name: key.name,
                                     type: key.type,
                                     allowedHosts: key.allowed_hosts
@@ -49,51 +48,70 @@ const KeyInfo = (keys) => {
                                 }}
                             >
                                 {({ status, isSubmitting }) => (
-                                    <div>
-                                        <Form className="key-form">
-                                            <div className="inline">
-                                                <label className="chronokeep-label" htmlFor="value">Key</label>
-                                                <Field name="value" type="text" disabled={true} className="chronokeep-input form-control" />
-                                                <label className="chronokeep-label" htmlFor="name">Name</label>
-                                                <Field name="name" type="text" disabled={true} id={`name${key.value}`} className="chronokeep-input form-control" />
-                                                <label className="chronokeep-label" htmlFor="type">Type</label>
-                                                <Field as="select" name="type" id={`type${key.value}`} disabled={true} className="form-select">
-                                                    <option value="read">Read</option>
-                                                    <option value="write">Write</option>
-                                                    <option value="delete">Delete</option>
-                                                </Field>
-                                                <label className="chronokeep-label" htmlFor="allowedHosts">Allowed Hosts</label>
-                                                <Field name="allowedHosts" type="text" disabled={true} id={`hosts${key.value}`} className="chronokeep-input form-control" />
-                                                {!isSubmitting &&
-                                                    <button type="submit" key={`submit${key.value}`} className="btn btn-primary btn-chronokeep" id={`submit${key.value}`} disabled={true}>
-                                                        <img src={save} alt="save name" />
-                                                    </button>
-                                                }
-                                                {isSubmitting &&
-                                                    <div className="spinner-border text-chronokeep" role="status">
-                                                        <span className="visually-hidden">Loading...</span>
+                                    <Form className="key-form">
+                                        <div className="card chronokeep-card key-card">
+                                            <h2 className="card-header h5 chronokeep-card-header text-center">{key.value}</h2>
+                                            <div className="card-body">
+                                                <div className="container">
+                                                    <div className="row justify-content-center align-items-end g-2">
+                                                        <div className="col-md-auto">
+                                                            <label className="chronokeep-label" htmlFor="name">Name</label>
+                                                            <Field name="name" type="text" disabled={true} id={`name${key.value}`} className="chronokeep-input form-control" />
+                                                        </div>
+                                                        <div className="col-md-auto">
+                                                            <label className="chronokeep-label" htmlFor="type">Type</label>
+                                                            <Field as="select" name="type" id={`type${key.value}`} disabled={true} className="form-select">
+                                                                <option value="read">Read</option>
+                                                                <option value="write">Write</option>
+                                                                <option value="delete">Delete</option>
+                                                            </Field>
+                                                        </div>
+                                                        <div className="col-md-auto">
+                                                            <label className="chronokeep-label" htmlFor="allowedHosts">Allowed Hosts</label>
+                                                            <Field name="allowedHosts" type="text" disabled={true} id={`hosts${key.value}`} className="chronokeep-input form-control" />
+                                                        </div>
+                                                        <div className="col-lg-auto">
+                                                            <div className="row g-2 justify-content-center">
+                                                                <div className="col-auto">
+                                                                    {!isSubmitting &&
+                                                                        <button type="submit" key={`submit${key.value}`} className="btn btn-primary btn-chronokeep" id={`submit${key.value}`} disabled={true}>
+                                                                            <img src={save} alt="save name" />
+                                                                        </button>
+                                                                    }
+                                                                    {isSubmitting &&
+                                                                        <div className="spinner-border text-chronokeep" role="status">
+                                                                            <span className="visually-hidden">Loading...</span>
+                                                                        </div>
+                                                                    }
+                                                                </div>
+                                                                <div className="col-auto">
+                                                                    <button type="button" key={`delete${key.value}`} id={`edit${key.value}`} disabled={true} onClick={()=>{
+                                                                        console.log(`deleting key value ${keyVal}`)
+                                                                    }} className="btn btn-primary btn-chronokeep">
+                                                                        <img src={trash} alt={`delete key ${key.value}`} />
+                                                                    </button>
+                                                                </div>
+                                                                <div className="col-auto">
+                                                                    <button type="button" key={`edit${key.value}`} onClick={()=>{
+                                                                        document.getElementById(`name${key.value}`).disabled = !document.getElementById(`name${key.value}`).disabled
+                                                                        document.getElementById(`type${key.value}`).disabled = !document.getElementById(`type${key.value}`).disabled
+                                                                        document.getElementById(`hosts${key.value}`).disabled = !document.getElementById(`hosts${key.value}`).disabled
+                                                                        document.getElementById(`edit${key.value}`).disabled = !document.getElementById(`edit${key.value}`).disabled
+                                                                        document.getElementById(`submit${key.value}`).disabled = !document.getElementById(`submit${key.value}`).disabled
+                                                                    }} className="btn btn-primary btn-chronokeep">
+                                                                        <img src={gear} alt={`edit key ${key.value}`} />
+                                                                    </button>
+                                                                </div>
+                                                            </div>
+                                                        </div>
                                                     </div>
+                                                </div>
+                                                {status &&
+                                                    <div className={'alert alert-danger chronokeep-alert'}>status</div>
                                                 }
-                                                <button type="button" key={`delete${key.value}`} id={`edit${key.value}`} disabled={true} onClick={()=>{
-                                                    console.log(`deleting key value ${keyVal}`)
-                                                }} className="btn btn-primary btn-chronokeep">
-                                                    <img src={trash} alt={`delete key ${key.value}`} />
-                                                </button>
-                                                <button type="button" key={`edit${key.value}`} onClick={()=>{
-                                                    document.getElementById(`name${key.value}`).disabled = !document.getElementById(`name${key.value}`).disabled
-                                                    document.getElementById(`type${key.value}`).disabled = !document.getElementById(`type${key.value}`).disabled
-                                                    document.getElementById(`hosts${key.value}`).disabled = !document.getElementById(`hosts${key.value}`).disabled
-                                                    document.getElementById(`edit${key.value}`).disabled = !document.getElementById(`edit${key.value}`).disabled
-                                                    document.getElementById(`submit${key.value}`).disabled = !document.getElementById(`submit${key.value}`).disabled
-                                                }} className="btn btn-primary btn-chronokeep">
-                                                    <img src={gear} alt={`edit key ${key.value}`} />
-                                                </button>
                                             </div>
-                                            {status &&
-                                                <div className={'alert alert-danger chronokeep-alert'}>status</div>
-                                            }
-                                        </Form>
-                                    </div>
+                                        </div>
+                                    </Form>
                                 )}
                             </Formik>
                         </div>
