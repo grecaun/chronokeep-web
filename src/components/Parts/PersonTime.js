9abcf59a52e67a2a8846694299d96b8e8c40ade1
James Sentinella <james@sentinella.me>
2022-03-27 15:19:54 -0700

    Added lap times to time based results display.

--- src/components/Parts/PersonTime.js
+++ src/components/Parts/PersonTime.js
@@ -12,6 +12,7 @@ class PersonTime extends Component {
 
     render() {
         const results = this.state.results;
+        var last = null;
         if (results.length > 0) {
             return (
                 <div className="container-lg lg-max-width m-4 mx-auto shadow p-5">
@@ -22,8 +23,8 @@ class PersonTime extends Component {
                             </tr>
                             <tr>
                                 <th></th>
-                                <th>Chip Time</th>
-                                <th>Gun Time</th>
+                                <th>Lap Time</th>
+                                <th>Total Time</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -33,11 +34,26 @@ class PersonTime extends Component {
                             if (segName === "Finish") {
                                 segName = "Lap " + res.occurence;
                             }
+                            var seconds = res.chip_seconds;
+                            var milliseconds = res.chip_milliseconds;
+                            if (last != null) {
+                                milliseconds = res.chip_milliseconds - last.chip_milliseconds
+                                seconds = res.chip_seconds - last.chip_seconds
+                                if (milliseconds < 0) {
+                                    milliseconds += 1000
+                                    seconds -= 1
+                                }
+                            }
+                            console.log(last)
+                            console.log(seconds)
+                            console.log(milliseconds)
+                            console.log("break")
+                            last = res
                             return(
                                 <tr key={`segment${index}`}>
                                     <td>{segName}</td>
+                                    <td>{FormatTime(seconds, milliseconds, res)}</td>
                                     <td>{FormatTime(res.chip_seconds, res.chip_milliseconds, res)}</td>
-                                    <td>{FormatTime(res.seconds, res.milliseconds, res)}</td>
                                 </tr>
                             );
                         })
