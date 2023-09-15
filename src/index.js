43eeaa453b8b4678d60ce1ddf6f16cbe2b8e8e75
James Sentinella <james@sentinella.me>
2023-09-15 11:14:46 -0700

    Minor css/js fixes.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './style.css';
 import { BrowserRouter } from 'react-router-dom';
 import ReactDomClient from 'react-dom/client';
