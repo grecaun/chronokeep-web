0f3c016781cd45c3f34e6436612b9f07405ecb83
James Sentinella <james@sentinella.me>
2024-06-01 15:41:55 -0700

    Modified age displays to not display if the age is under 1 or over 129.

--- src/Parts/TimeResultsTable.tsx
+++ src/Parts/TimeResultsTable.tsx
@@ -115,7 +115,7 @@ class TimeResultsTable extends Component<ResultsTableProps> {
                                             <td className="overflow-hidden-sm text-center">{result.bib}</td>
                                             <td className="text-center">{rankStr}</td>
                                             <td><Link to={`/results/${info.slug}/${info.year}/${result.bib}`} className="nav-link m-0 p-0">{`Bib ${result.bib}`}</Link></td>
-                                            <td className="overflow-hidden-lg text-center">{result.age}</td>
+                                            <td className="overflow-hidden-lg text-center">{result.age > 0 && result.age < 130 ? result.age : ""}</td>
                                             <td className="overflow-hidden-lg text-center">{arankStr}</td>
                                             <td className="overflow-hidden-lg text-center">{result.gender}</td>
                                             <td className="overflow-hidden-lg text-center">{grankStr}</td>
