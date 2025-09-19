863b49b64a69cfaade80bf01273beab11ce14593
James Sentinella <james@sentinella.me>
2025-09-19 12:45:47 -0700

    Initial work on a results page for team events.

---
+++ src/Pages/TeamResults.tsx
@@ -0,0 +1,286 @@
+import Loading from '../Parts/Loading';
+import ErrorMsg from '../Parts/ErrorMsg';
+import DateString from '../Parts/DateString';
+import { useParams } from 'react-router-dom';
+import { ResultsLoader } from '../loaders/results';
+import { TimeResult } from '../Interfaces/types';
+import PNTFAwardsTable from '../Parts/PNTFAwardsTable';
+
+function TeamResults() {
+    const params = useParams();
+    const { state } = ResultsLoader(params, 'results');
+    document.title = `Chronokeep - Results`
+    if (state.error === true) {
+        document.title = `Chronokeep - Error`
+        return (
+            <ErrorMsg status={state.status} message={state.message} />
+        );
+    }
+    if (state.loading === true) {
+        return (
+            <Loading />
+        );
+    }
+    const info = {
+        slug: params.slug,
+        year: state.year?.year
+    }
+    var distances = Object.keys(state.results)
+    const results: { [index: string]: TimeResult[] } = {}
+    const teamResults: { [index: string]: { [index: string]: TimeResult[] } } = {}
+    const teamCount: {  [index: string]: { [index:string]: { members: number, women: number } } } = {}
+    distances.map(distance => {
+        state.results[distance].map(result => {
+            if (result.division.trim().length > 0 && result.finish === true) {
+                if (teamCount[result.distance] === undefined) {
+                    teamCount[result.distance] = {}
+                }
+                if (teamCount[result.distance][result.division] === undefined) {
+                    teamCount[result.distance][result.division] = { members: 0, women: 0 }
+                }
+                teamCount[result.distance][result.division].members += 1
+                result.gender = result.gender.toLocaleUpperCase();
+                result.gender = result.gender.substring(0,2)
+                if (result.gender === "F" || result.gender === "WO" || result.gender === "W") {
+                    result.gender = "W"
+                    teamCount[result.distance][result.division].women += 1
+                } else if (result.gender === "M" || result.gender === "MA") {
+                    result.gender = "M"
+                } else {
+                    result.gender = "X"
+                }
+                teamCount[result.division].women
+                if (results[distance] === undefined) {
+                    results[distance] = []
+                }
+                results[distance].push({
+                        bib: result.bib,
+                        first: result.first,
+                        last: result.last,
+                        seconds: result.seconds,
+                        milliseconds: result.milliseconds,
+                        chip_seconds: result.chip_seconds,
+                        chip_milliseconds: result.chip_milliseconds,
+                        gender: result.gender,
+                        occurence: result.occurence,
+                        age_group: result.age_group,
+                        age: result.age,
+                        ranking: result.ranking,
+                        age_ranking: result.age_ranking,
+                        gender_ranking: result.gender_ranking,
+                        finish: result.finish,
+                        segment: result.segment,
+                        type: result.type,
+                        anonymous: result.anonymous,
+                        distance: result.distance,
+                        location: result.location,
+                        local_time: result.local_time,
+                        division: result.division,
+                        division_ranking: result.division_ranking
+                    })
+            }
+        })
+    })
+    // Update rankings to only use rankings for finishers of the team event.
+    distances = Object.keys(results)
+    distances.map(distance => {
+        results[distance].sort((a,b) => {
+            return a.ranking - b.ranking;
+        })
+        var place = 1;
+        var genderPlace: { [gend: string]: number } = {}
+        var ageGroupPlace: { [age_group: string]: { [gend: string]: number } } = {}
+        var divisionPlace: { [age_group: string]: { [gend: string]: number } } = {}
+        results[distance].map(result => {
+            // Limit rankings to only teams with at least 4 finishers, 2 of which must be women
+            if (teamCount[distance] !== undefined && teamCount[distance][result.division] !== undefined &&
+                teamCount[distance][result.division].members >= 4 && teamCount[distance][result.division].women >= 2) {
+                result.ranking = place;
+                place = place + 1;
+                if (genderPlace[result.gender] === undefined) {
+                    result.gender_ranking = 1;
+                    genderPlace[result.gender] = 2;
+                } else {
+                    result.gender_ranking = genderPlace[result.gender];
+                    genderPlace[result.gender] = genderPlace[result.gender] + 1;
+                }
+                if (ageGroupPlace[result.age_group] === undefined) {
+                    result.age_ranking = 1;
+                    ageGroupPlace[result.age_group] = {};
+                    ageGroupPlace[result.age_group][result.gender] = 2;
+                } else if (ageGroupPlace[result.age_group][result.gender] === undefined) {
+                    result.age_ranking = 1;
+                    ageGroupPlace[result.age_group][result.gender] = 2;
+                } else {
+                    result.age_ranking = ageGroupPlace[result.age_group][result.gender];
+                    ageGroupPlace[result.age_group][result.gender] = ageGroupPlace[result.age_group][result.gender] + 1;
+                }
+                if (divisionPlace[result.division] === undefined) {
+                    result.division_ranking = 1
+                    divisionPlace[result.division] = {}
+                    divisionPlace[result.division][result.gender] = 2
+                } else if (divisionPlace[result.division][result.gender] === undefined) {
+                    result.division_ranking = 1
+                    divisionPlace[result.division][result.gender] = 2
+                } else {
+                    result.division_ranking = divisionPlace[result.division][result.gender]
+                    divisionPlace[result.division][result.gender] = divisionPlace[result.division][result.gender] + 1
+                }
+                if (teamResults[distance] === undefined) {
+                    teamResults[distance] = {}
+                }
+                if (teamResults[distance][result.division] === undefined) {
+                    teamResults[distance][result.division] = []
+                }
+                teamResults[distance][result.division].push({
+                        bib: result.bib,
+                        first: result.first,
+                        last: result.last,
+                        seconds: result.seconds,
+                        milliseconds: result.milliseconds,
+                        chip_seconds: result.chip_seconds,
+                        chip_milliseconds: result.chip_milliseconds,
+                        gender: result.gender,
+                        occurence: result.occurence,
+                        age_group: result.age_group,
+                        age: result.age,
+                        ranking: result.ranking,
+                        age_ranking: result.age_ranking,
+                        gender_ranking: result.gender_ranking,
+                        finish: result.finish,
+                        segment: result.segment,
+                        type: result.type,
+                        anonymous: result.anonymous,
+                        distance: result.distance,
+                        location: result.location,
+                        local_time: result.local_time,
+                        division: result.division,
+                        division_ranking: result.division_ranking
+                    })
+            }
+        })
+    })
+    const teamPoints: {  [index: string]:
+                        { [index:string]:
+                            { ranking: number,
+                                points: number,
+                                firstWoman: TimeResult | undefined,
+                                secondWoman: TimeResult | undefined,
+                                thirdResult: TimeResult | undefined,
+                                fourthResult: TimeResult | undefined }
+                            }
+                        } = {}
+    const teamPlacements: {  [index: string]:
+                            { ranking: number,
+                                points: number,
+                                firstWoman: TimeResult | undefined,
+                                secondWoman: TimeResult | undefined,
+                                thirdResult: TimeResult | undefined,
+                                fourthResult: TimeResult | undefined }[]
+                        } = {}
+    distances.map(distance => {
+        teamPoints[distance] = {}
+        teamPlacements[distance] = []
+        const teams = Object.keys(teamResults[distance])
+        teams.map(team => {
+            teamPoints[distance][team] =
+            {
+                ranking: 0,
+                points: 0,
+                firstWoman: undefined,
+                secondWoman: undefined,
+                thirdResult: undefined,
+                fourthResult: undefined
+            }
+            teamResults[distance][team].map(res => {
+                if (teamPoints[distance][team].firstWoman === undefined && res.gender === "W") {
+                    teamPoints[distance][team].firstWoman = res
+                    teamPoints[distance][team].points += res.ranking
+                } else if (teamPoints[distance][team].secondWoman === undefined && res.gender === "W") {
+                    teamPoints[distance][team].secondWoman = res
+                    teamPoints[distance][team].points += res.ranking
+                } else if (teamPoints[distance][team].thirdResult === undefined) {
+                    teamPoints[distance][team].thirdResult = res
+                    teamPoints[distance][team].points += res.ranking
+                } else if (teamPoints[distance][team].fourthResult === undefined) {
+                    teamPoints[distance][team].fourthResult = res
+                    teamPoints[distance][team].points += res.ranking
+                }
+            })
+            teamPlacements[distance].push(teamPoints[distance][team])
+        })
+        teamPlacements[distance].sort((a,b) => {
+            if (a.firstWoman !== undefined && a.secondWoman !== undefined && a.thirdResult && a.fourthResult &&
+                b.firstWoman !== undefined && b.secondWoman !== undefined && b.thirdResult && b.fourthResult) {
+                return a.points - b.points
+            }
+            if (a.firstWoman !== undefined && a.secondWoman !== undefined && a.thirdResult && a.fourthResult) {
+                return 1
+            }
+            return -1
+        })
+        var place = 1
+        teamPlacements[distance].map(teamPlace => {
+            if (teamPlace.firstWoman !== undefined && teamPlace.secondWoman !== undefined && teamPlace.thirdResult && teamPlace.fourthResult) {
+                teamPlace.ranking = place
+                place += 1
+            }
+        })
+    })
+    const pageSubTitle = 'Team Rankings'
+    document.title = `Chronokeep - ${state.event!.name} - ${pageSubTitle}`
+    return (
+        <div>
+            <div className="row container-lg lg-max-width mx-auto d-flex mt-4 mb-3 align-items-stretch">
+                <div className="col-md-10 flex-fill text-center mx-auto m-1">
+                    <p className="text-important mb-0 mt-1 h1">{`${state.event!.name}`}</p>
+                    <p className="text-important mb-2 mt-0 h2">{pageSubTitle}</p>
+                    <p className="text-important h5">{DateString(state.year!.date_time)}</p>
+                </div>
+            </div>
+            { distances.length > 0 &&
+            <div>
+                <div className="row container-lg lg-max-width mx-auto d-flex align-items-stretch shadow-sm p-0 mb-3 border border-light">
+                    <div className="p-0">
+                        <ul className="nav nav-tabs nav-fill">
+                            { distances.length > 1 &&
+                                distances.map((distance, index) => {
+                                    return (
+                                        <li className="nav-item" key={`distance${index}`}>
+                                            <a className="nav-link text-important h5 text-secondary" href={`#${distance}`} role="button">{distance}</a>
+                                        </li>
+                                    );
+                                })
+                            }
+                        </ul>
+                        <div id="results-parent">
+                            { distances.map((distance, index) => {
+                                    return (
+                                        <PNTFAwardsTable
+                                            info={info}
+                                            distance={distance}
+                                            results={results[distance]}
+                                            key={index}
+                                            show_title={distances.length > 1}
+                                            />
+                                    )
+                                })
+                            }
+                        </div>
+                    </div>
+                </div>
+                <div id='disclaimer' className='container-lg lg-max-width shadow-sm text-center p-3 mb-3 border border-light overflow-hidden-sm'>{"*Rankings are ranked based upon the Clock Time and not the Chip Time."}</div>
+            </div>
+            }
+            { distances.length === 0 &&
+            <div className="container-lg lg-max-width shadow-sm p-5 mb-3 border border-light">
+                <div className="text-center">
+                    <h2>No results to display.</h2>
+                </div>
+            </div>
+            }
+        </div>
+    )
+}
+
+export default TeamResults;
